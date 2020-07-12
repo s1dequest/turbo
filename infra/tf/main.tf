a185b7efcf6cdc82338dc765ffac238b03b1db27
s1dequest <maxzintel@gmail.com>
2020-07-12 18:05:48 -0400

    Add sample eks resource and relevant outputs

--- infra/tf/main.tf
+++ infra/tf/main.tf
@@ -1,38 +1,22 @@
 // reference: https://www.terraform.io/docs/providers/aws/index.html
-variable "ami" {
-  type = map
-  default = {
-    "us-east-1" = "ami-b374d5a5" // then refer to this in main.tf with var.amis[var.region]
-    "us-west-2" = "ami-4b32be2b"
+resource "aws_eks_cluster" "turbo-v1" {
+  name     = "turbo"
+  role_arn = "value"
+
+  vpc_config {
+    subnet_ids = ["value"]
   }
+
+  depends_on = [
+    "aws_iam_role_policy_attachment.AWSEKSClusterPolicy",
+    "aws_iam_role_policy_attachment.AWSEKSServicePolicy"
+  ]
 }
 
-variable "region" {}
-
-provider "aws" {
-  profile = "default" // From your ~/.aws/config file. This is the profile for the account you want to provision infrastructure for.
-  region = var.region
+output "endpoint" {
+  value = "${aws_eks_cluster.turbo-v1.endpoint}"
 }
 
-resource "aws_instance" "example" { // where example = the name of the instance.
-  ami           = var.ami[var.region]
-  instance_type = "t2.micro"
-
-  provisioner "local-exec" { // provisioners are used to define multiple provisioning steps. 
-    command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
-  }
-}
-
-resource "aws_instance" "another" { // where example = the name of the instance.
-  ami           = var.ami[var.region]
-  instance_type = "t2.micro"
-}
-
-resource "aws_eip" "ip" {
-  vpc      = true
-  instance = "aws_instance.example.id"
-}
-
-output "ami" {
-  value = aws_instance.example.ami
+output "kubeconfig-certificate-authority-data" {
+  value = "${aws_eks_cluster.turbo-v1.certificate_authority.0.data}"
 }
