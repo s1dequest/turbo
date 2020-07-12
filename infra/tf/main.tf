07ea658c62947beb5a43c07d9a604367a0409e31
s1dequest <maxzintel@gmail.com>
2020-07-12 17:48:47 -0400

    Messing with terraform more to better understand how vars are passed through

--- infra/tf/main.tf
+++ infra/tf/main.tf
@@ -1,11 +1,21 @@
 // reference: https://www.terraform.io/docs/providers/aws/index.html
+variable "ami" {
+  type = map
+  default = {
+    "us-east-1" = "ami-b374d5a5" // then refer to this in main.tf with var.amis[var.region]
+    "us-west-2" = "ami-4b32be2b"
+  }
+}
+
+variable "region" {}
+
 provider "aws" {
   profile = "default" // From your ~/.aws/config file. This is the profile for the account you want to provision infrastructure for.
-  region  = "us-east-1"
+  region = var.region
 }
 
 resource "aws_instance" "example" { // where example = the name of the instance.
-  ami           = "ami-2757f631"
+  ami           = var.ami[var.region]
   instance_type = "t2.micro"
 
   provisioner "local-exec" { // provisioners are used to define multiple provisioning steps. 
@@ -14,11 +24,15 @@ resource "aws_instance" "example" { // where example = the name of the instance.
 }
 
 resource "aws_instance" "another" { // where example = the name of the instance.
-  ami           = "ami-2757f631"
+  ami           = var.ami[var.region]
   instance_type = "t2.micro"
 }
 
 resource "aws_eip" "ip" {
-  vpc = true
+  vpc      = true
   instance = "aws_instance.example.id"
+}
+
+output "ami" {
+  value = aws_instance.example.ami
 }
