be1157170719dae659de456db7ab1f324973b5d9
s1dequest <maxzintel@gmail.com>
2020-07-12 17:16:53 -0400

    More experimenting with terraforms capabilities

--- infra/tf/main.tf
+++ infra/tf/main.tf
@@ -7,4 +7,18 @@ provider "aws" {
 resource "aws_instance" "example" { // where example = the name of the instance.
   ami           = "ami-2757f631"
   instance_type = "t2.micro"
+
+  provisioner "local-exec" { // provisioners are used to define multiple provisioning steps. 
+    command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+  }
+}
+
+resource "aws_instance" "another" { // where example = the name of the instance.
+  ami           = "ami-2757f631"
+  instance_type = "t2.micro"
+}
+
+resource "aws_eip" "ip" {
+  vpc = true
+  instance = "aws_instance.example.id"
 }
