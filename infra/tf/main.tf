6085488b622973d848392d0b0fe52ec5840ec6f8
s1dequest <maxzintel@gmail.com>
2020-07-12 18:18:58 -0400

    Add iam role and attach relevant policies.

--- infra/tf/main.tf
+++ infra/tf/main.tf
@@ -20,3 +20,33 @@ output "endpoint" {
 output "kubeconfig-certificate-authority-data" {
   value = "${aws_eks_cluster.turbo-v1.certificate_authority.0.data}"
 }
+
+resource "aws_iam_role" "turbo-v1-eks-role" {
+  name = "turbo-eks-role"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+// referenced as a dependency in the turbo-v1 eks cluster resource.
+resource "aws_iam_role_policy_attachment" "AWSEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/aws-service-role/AmazonEKSClusterPolicy"
+  role       = "${aws_iam_role.turbo-v1-eks-role.turbo-eks-role}"  
+}
+
+resource "aws_iam_role_policy_attachment" "AWSEKSServicePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/aws-service-role/AmazonEKSServicePolicy"
+  role       = "${aws_iam_role.turbo-v1-eks-role.turbo-eks-role}"  
+}
