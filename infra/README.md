c541c4dbe7e35af58fee69744e8542b4005f0136
s1dequest <maxzintel@gmail.com>
2020-07-24 11:55:06 -0400

    Removing a bunch of stuff to get base level cluster running

--- infra/README.md
+++ infra/README.md
@@ -9,3 +9,18 @@
 7. To inspect your state, check the file terraform writes to: `terraform.tfstate`. This file contains the IDs and properties of the resources we've created so that it can manage or destroy those resources going forward. **STORE THIS FILE REMOTELY IN PROD**; it both ensures security and enables collaboration.
 8. Inspect current state in the terminal with: `terraform show`
 9. Manually manage state using: `terraform state`, ex: to list resources managed by terraform, run `terraform state list`.
+  
+---------------
+  
+Take customer inputs via questions pertaining to Architecture tradeoffs.
+* What is your monthly budget?
+  * Run some script to predict monthly costs based on resources we are provisioning.
+  * This will determine the instance type we recommend, as well as what additional resources we can fit in the cluster.
+* Is high throughput a priority?
+  * This is generally something that has to be 'bought' through more expensive EC2's.
+  * If the answer is no, we can save a lot of money by using smaller instances.
+* Is low latency a priority?
+  * If yes, geographical proximity to the end user is paramount, and thus we would want to provision a CDN like Cloudflare.
+
+**To make deployments dynamic based on customer inputs:**
+* Use a combo of jq in a shell script to grab env vars from bash, OR separate terraform files depending on certain things like if, based on the customer inputs, certain resources are included or excluded in the deployment and thus changes in the deployment are more substantial than string values that can be injected via jq.
--- infra/tf/cluster.tf
+++ infra/tf/cluster.tf
@@ -2,49 +2,73 @@
 module "eks" {
   source       = "terraform-aws-modules/eks/aws"
   cluster_name = local.cluster_name
-  role_arn  = aws_iam_role.iam-eks-role.arn
-  vpc_id    = module.vpc.vpc_id
+  vpc_id       = module.vpc.vpc_id
+  subnets      = module.vpc.private_subnets
 
-  depends_on = [
-    aws_iam_role_policy_attachment.AWSEKSClusterPolicy,
-    aws_iam_role_policy_attachment.AWSEKSServicePolicy
-  ]
-}
+  tags = {
+    Environment = "test"
+  }
 
-output "endpoint" {
-  value = aws_eks_cluster.turbo-cluster.endpoint
-}
-
-output "kubeconfig-certificate-authority-data" {
-  value = aws_eks_cluster.turbo-cluster.certificate_authority.0.data
-}
-
-resource "aws_iam_role" "iam-eks-role" {
-  name = "turbo-cluster-eks-role"
-
-  assume_role_policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
+  worker_groups = [
     {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "eks.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
+      name                          = "worker-group-1"
+      instance_type                 = "t2.small"
+      additional_userdata           = "echo foo bar"
+      asg_desired_capacity          = 2
+      additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
+    },
+    {
+      name                          = "worker-group-2"
+      instance_type                 = "t2.small"
+      additional_userdata           = "echo foo bar"
+      asg_desired_capacity          = 2
+      additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
     }
   ]
 }
-POLICY
+
+# output "endpoint" {
+#   value = module.eks.endpoint
+# }
+
+# output "kubeconfig-certificate-authority-data" {
+#   value = module.eks.certificate_authority.0.data
+# }
+
+# resource "aws_iam_role" "iam-eks-role" {
+#   name = "turbo-cluster-eks-role"
+
+#   assume_role_policy = <<POLICY
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Effect": "Allow",
+#       "Principal": {
+#         "Service": "eks.amazonaws.com"
+#       },
+#       "Action": "sts:AssumeRole"
+#     }
+#   ]
+# }
+# POLICY
+# }
+
+# // referenced as a dependency in the "turbo-cluster" eks cluster resource.
+# resource "aws_iam_role_policy_attachment" "AWSEKSClusterPolicy" {
+#   policy_arn = "arn:aws:iam::aws:policy/aws-service-role/AmazonEKSClusterPolicy"
+#   role       = aws_iam_role.iam-eks-role.name
+# }
+
+# resource "aws_iam_role_policy_attachment" "AWSEKSServicePolicy" {
+#   policy_arn = "arn:aws:iam::aws:policy/aws-service-role/AmazonEKSServicePolicy"
+#   role       = aws_iam_role.iam-eks-role.name
+# }
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
 }
 
-// referenced as a dependency in the "turbo-cluster" eks cluster resource.
-resource "aws_iam_role_policy_attachment" "AWSEKSClusterPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/aws-service-role/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.iam-eks-role.name
-}
-
-resource "aws_iam_role_policy_attachment" "AWSEKSServicePolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/aws-service-role/AmazonEKSServicePolicy"
-  role       = aws_iam_role.iam-eks-role.name
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
 }
