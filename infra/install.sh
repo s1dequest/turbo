2f11841d644167897f96822e4858aa7fa5cff261
s1dequest <maxzintel@gmail.com>
2020-08-03 15:21:13 -0400

    Make install script functional

--- infra/install.sh
+++ infra/install.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 set -e
 
-export GIT_COMMIT=$(git log -1 --format=%h)
-export CLUSTER_NAME=$(jq -r '.clusterName' ./tf/terraform.tfvars.json | tr -d "[:cntrl:]")
-echo $CLUSTER_NAME
+# export GIT_COMMIT=$(git log -1 --format=%h)
+export BASE_CLUSTER_NAME=$(jq -r '.clusterName' ./tf/terraform.tfvars.json | tr -d "[:cntrl:]")
 export REGION=$(jq -r '.region' ./tf/terraform.tfvars.json | tr -d "[:cntrl:]")
-echo $REGION
 
+# Run Terraform, apply any changes.
 terraform init
 terraform apply
+
+# Grab cluster name from EKS. NOTE: Will only work if there is only 1 eks cluster in the account.
+export FULL_NAME=$(aws eks list-clusters | jq -r '.clusters[0]') # Includes random string.
+
+aws eks --region ${REGION} update-kubeconfig --name ${FULL_NAME}
