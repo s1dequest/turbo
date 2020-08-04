caba517690f3aabeb1510d41b71eba95d96897da
s1dequest <maxzintel@gmail.com>
2020-08-04 13:44:24 -0400

    Fix frontend yaml, add new terraform backend. Edit Dockerfile to try to fix redis module error on docker run.

--- infra/tf/backend.tf
+++ infra/tf/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket  = "turbo-remote-backend"
-    key     = "turbo-v1"
+    key     = "turbo-v1.1"
     region  = "us-east-1"
     profile = "terraform-remote-backend"
   }
