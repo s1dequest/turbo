891a584091af8a470e777e503cb42386df688c69
s1dequest <maxzintel@gmail.com>
2020-08-10 16:40:27 -0400

    Add comments to destroy script

--- infra/destroy.sh
+++ infra/destroy.sh
@@ -2,6 +2,11 @@
 set -e
 cd tf/
 
+echo
+echo "Deleting your ingress controller and associated resources."
+echo "If we do not do this step, the Terraform destroy will not work."
 helm delete ingress-nginx-v1
 
+echo
+echo "Terraform destroy."
 terraform destroy
