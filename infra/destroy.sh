43922e49cabacb6f38a2299d00acd467e7e1c928
s1dequest <maxzintel@gmail.com>
2020-08-21 15:11:23 -0400

    Add remove prometheus, +1 oxymoron

--- infra/destroy.sh
+++ infra/destroy.sh
@@ -3,6 +3,11 @@ set -e
 cd tf/
 
 echo
+echo "Deleting your Prometheus monitoring stack and associated resources."
+echo "If we do not do this step, the Terraform destroy will not work."
+helm delete prom
+
+echo
 echo "Deleting your ingress controller and associated resources."
 echo "If we do not do this step, the Terraform destroy will not work."
 helm delete ingress-nginx-v1
