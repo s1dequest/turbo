508f9d3be1f4f985f60c5954ff760c7641133fab
s1dequest <maxzintel@gmail.com>
2020-08-21 15:10:21 -0400

    Add prometheus

--- infra/install.sh
+++ infra/install.sh
@@ -5,8 +5,12 @@ cd tf/
 export REGION=$(jq -r '.region' ./terraform.tfvars.json | tr -d "[:cntrl:]")
 
 # Run Terraform, apply any changes.
+echo
 echo "Now initializing your Terraform backend and applying the changes required..."
 echo "This will take about 12 minutes."
+echo "."
+echo "."
+echo "."
 terraform init
 terraform apply
 
@@ -16,10 +20,24 @@ export FULL_NAME=$(aws eks list-clusters | jq -r '.clusters[0]') # Includes rand
 # Update your kubeconfig to access the new cluster.
 echo 
 echo "Updating your kubeconfig to access the new cluster."
+echo "."
+echo "."
+echo "."
 aws eks --region ${REGION} update-kubeconfig --name ${FULL_NAME}
 
 # Add bitnami charts and install nginx ingress controller.
 echo
 echo "Adding Bitnami's helm chart for nginx ingress."
+echo "."
+echo "."
+echo "."
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm install ingress-nginx-v1 bitnami/nginx
+
+echo
+echo "Adding Prometheus Operator for configuring cluster monitoring."
+echo "."
+echo "."
+echo "."
+helm repo add stable https://kubernetes-charts.storage.googleapis.com
+helm install prom stable/prometheus-operator
